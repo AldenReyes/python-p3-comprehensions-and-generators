ea75dd1e56c8df8af9a4a3ba75061342bac6d28e
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-07-23 01:35:07 -0700

    Completes lab: explores the usage of list comprehensions and generator expressions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
-    pass
+    """Returns a list of even numbers from an inputted list"""
+    return [n for n in num_list if n % 2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    """Adds an exclamation mark to each string element from an inputted list"""
+    exclamation_list = list((s + "!") for s in sentence_list)
+    return exclamation_list
